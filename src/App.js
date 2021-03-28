a2a5a17524e6bf1273eed6496af9a20e48948cbc
Saima Khan <tithi888khan@gmail.com>
2021-03-29 02:17:14 +0600

    Created a layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, { component } from 'react';
+
+import Layout from './components/Layout/Layout'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
