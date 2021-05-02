c69cb8c9b0f77f05f0722c8637b7f1c55935d1a1
Saima Khan <tithi888khan@gmail.com>
2021-05-02 16:36:35 +0600

    Added logo and navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem' 
+
+const navigationItems = (props) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>
+            Buger Builder
+        </NavigationItem>
+        <NavigationItem link="/">
+            Checkout
+        </NavigationItem>
+    </ul>
+
+)
+
+export default navigationItems
